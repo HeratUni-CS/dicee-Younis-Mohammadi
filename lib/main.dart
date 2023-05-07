8af27e0d0ac5499226e515afdb3b35c30a1e7cf7
Mohammad Younis <artacodding2023@gmail.com>
2023-05-07 03:57:59 -0700

    all

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan.shade900,
+        appBar: AppBar(
+          title: Text('Dice'),
+          backgroundColor: Colors.cyan.shade900,
+        ),
+        body: DicePage(),
+      ),
+    )
+  );
+}
+
+class DicePage extends StatefulWidget {
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber =5;
+  int rightDiceNumber =5;
+  @override
+  Widget build(BuildContext context) {
+    
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+                onPressed: (){
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6)+1;
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
+            ),
+          Expanded(
+              child: TextButton(
+                onPressed: (){
+                  setState(() {
+                    rightDiceNumber = Random().nextInt(6)+1;
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+                ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
